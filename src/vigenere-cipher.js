fdb0e4dfbd7b0fc83e6b7eb5b998b152dc27e209
Anna-Alinovskaya <alinovskaya_a@mail.ru>
2021-03-29 21:34:34 +0300

    feat: add vigenere-chipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,106 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    constructor(message) {
+      this.message = message;
+      this.words = 'abcdefghijklmnopqrstuvwxyz';
     }
+    encrypt(str, key) {
+      let letters = [];
+      let keys = [];
+      let encryptArr = [];
+      let result = [];
+      let string = str.toLowerCase().split('');
+      let key_string = key.toLowerCase().split('');
+      let count = 0;
+      let keyCount = 0;
+  
+      if(!str || !key) throw new Error();
+      if(this.message === undefined) this.message = true;
+  
+      for(let i = 0; i < string.length; i++) {
+        if(this.words.indexOf(string[i]) != -1) {
+          letters.push(string[i]);
+        }
+      };
+      for(let i = 0; i < letters.length; i++) {
+        if(keyCount < key_string.length) {
+          keys.push(key_string[keyCount]);
+          keyCount += 1;
+        } else {
+          keyCount = 0;
+          keys.push(key_string[keyCount]);
+          keyCount += 1;
+        }
+      };
+      letters.forEach((l, i) => {
+        if (this.words.indexOf(l) + this.words.indexOf(keys[i]) >= 26) {
+            encryptArr.push(this.words.charAt(this.words.indexOf(l) + this.words.indexOf(keys[i]) - 26))
+        } else {
+            encryptArr.push(this.words.charAt(this.words.indexOf(l) + this.words.indexOf(keys[i])))
+        }
+      });
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+      string.forEach((l, i) => {
+        let el = this.words.indexOf(l);
+        if(el === -1) {
+          count += 1;
+          result.push(l);
+        } else {
+          result.push(encryptArr[i - count]);
+        }
+      })
+      if(!this.message) result.reverse();
+  
+      return result.join('').toUpperCase();
+  }    
+  
+    decrypt(str, key) {
+      let letters = [];
+      let keys = [];
+      let encryptArr = [];
+      let result = [];
+      let string = str.toLowerCase().split('');
+      let key_string = key.toLowerCase().split('');
+      let count = 0;
+      let keyCount = 0;
+  
+      if(!str || !key) throw new Error();
+      if(this.message === undefined) this.message = true;
+  
+      for(let i = 0; i < string.length; i++) {
+        if(this.words.indexOf(string[i]) != -1) {
+          letters.push(string[i]);
+        }
+      };
+      for(let i = 0; i < letters.length; i++) {
+        if(keyCount < key_string.length) {
+          keys.push(key_string[keyCount]);
+          keyCount += 1;
+        } else {
+          keyCount = 0;
+          keys.push(key_string[keyCount]);
+          keyCount += 1;
+        }
+      };
+  
+      letters.forEach((l, i) => {
+        if (this.words.indexOf(l) - this.words.indexOf(keys[i]) < 0) {
+            encryptArr.push(this.words.charAt(this.words.indexOf(l) - this.words.indexOf(keys[i]) + 26))
+        } else {
+            encryptArr.push(this.words.charAt(this.words.indexOf(l) - this.words.indexOf(keys[i])))
+        }
+      });
+  
+      string.forEach((l, i) => {
+        let el = this.words.indexOf(l);
+        if(el == -1) {
+          count += 1;
+          result.push(l);
+        } else {
+          result.push(encryptArr[i - count]);
+        }
+      })
+      if(!this.message) result.reverse();
+      return result.join('').toUpperCase();
     }
 }
-
 module.exports = VigenereCipheringMachine;
--- test/transform-array.test.js
+++ test/transform-array.test.js
@@ -1,4 +1,4 @@
-const chai = require('chai');
+/*const chai = require('chai');
 const { expect, assert } = chai;
 it.optional = require('../extensions/it-optional');
 
@@ -47,3 +47,4 @@ describe('Transform array', () => {
     });
 });
 
+*/
